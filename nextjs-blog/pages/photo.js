8a3d709962760478fa44c3b1c324cb090bcbf9ca
HandyDorceus <77027304+itisjamied@users.noreply.github.com>
2023-06-20 17:41:56 -0400

    Set up all js files for studio home pages

---
+++ nextjs-blog/pages/photo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/studioHome.module.css";
+
+
+
+const studioHome = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>AFH ONLINE</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+            <div className={styles.title}> This is the Photo Page </div>
+
+        </div >
+    );
+};
+
+export default studioHome;
