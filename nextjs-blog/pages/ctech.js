151d97ba69060c374a7e6ac84c28034a21426534
HandyDorceus <77027304+itisjamied@users.noreply.github.com>
2023-06-21 14:31:36 -0400

    Implemented studioHome component  on all pages

--- nextjs-blog/pages/ctech.js
+++ nextjs-blog/pages/ctech.js
@@ -1,22 +1,131 @@
 import React from "react";
-import Head from "next/head";
+import StudioHome from "../components/StudioHome";
+import MentorCard from "../components/MentorCard";
+import LessonCard from "../components/LessonCard";
 import styles from "../styles/studioHome.module.css";
+import Image from 'next/image';
 
 
 
-const studioHome = () => {
+
+const ctechHome = () => {
     return (
         <div className={styles.container}>
-            <Head>
-                <title>AFH ONLINE</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+
+            <StudioHome
+                studio={"Creative Technology"}
+                description={"a field encompassing art, digital product design, digital media or an advertising and media made with a software-based, electronic and/or data-driven engine."}
+            />
+
+            <div className={styles.subheader}> Meet the Mentors </div>
+            <div className={styles.mentorCards}>
+                <MentorCard
+                    first="Handy"
+                    last="Dorceus"
+                    role="Coding Director"
+                    headshot="/images/profile.png"
+                />
+                <MentorCard
+                    first="Trevor"
+                    last="Ward"
+                    role="Animation Director"
+                    headshot="/images/profile.png"
+
+                />
+
+                <MentorCard
+                    first="Jamie"
+                    last="De Los Santos"
+                    role="Web Dev Assistant Mentor"
+                    headshot="/images/profile.png"
+                />
+
+                <MentorCard
+                    first="Luke"
+                    last="Chen"
+                    role="User Design Assitant Mentor"
+                    headshot="/images/profile.png"
+                />
+
+                <Image
+                    src="/images/forwardArrow.png"
+                    width={50}
+                    height={50}
+                    alt="Forward Arrow"
+                    className={styles.arrow}
+                />
+            </div>
+
+            <div className={styles.workshop}> Our Workshops </div>
+
+            <div className={styles.lessonCards}>
+                <LessonCard
+                    studiochip="Graphic Design"
+                    title="Doodle-to-Vector"
+                    author="Mikey Guderama"
+                    caption="Unleash your inner doodler and learn how to transform your sketches into vibrant and scalable vector graphics."
+                />
+                <LessonCard
+                    studiochip="Graphic Design"
+                    title="Doodle-to-Vector"
+                    author="Mikey Guderama"
+                    caption="Unleash your inner doodler and learn how to transform your sketches into vibrant and scalable vector graphics."
+                />
+                <LessonCard
+                    studiochip="Graphic Design"
+                    title="Doodle-to-Vector"
+                    author="Mikey Guderama"
+                    caption="Unleash your inner doodler and learn how to transform your sketches into vibrant and scalable vector graphics."
+                />
+                <LessonCard
+                    studiochip="Graphic Design"
+                    title="Doodle-to-Vector"
+                    author="Mikey Guderama"
+                    caption="Unleash your inner doodler and learn how to transform your sketches into vibrant and scalable vector graphics."
+                />
 
 
-            <div className={styles.title}> This is the C-tech Page </div>
+            </div>
 
+            <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI,
+            Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+            Helvetica Neue, sans-serif;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+
+
+        h1,
+        h2,
+        p,
+        ul {
+          margin: 0;
+        }
+
+        ul {
+          padding: 0;
+          list-style: none;
+        }
+
+        button {
+          padding: 0.5rem 1rem;
+          font-weight: bold;
+        }
+      `}</style>
         </div >
     );
 };
 
-export default studioHome;
+export default ctechHome;
